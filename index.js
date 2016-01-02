e43dd66df6888a66e2a0f489fc1e9385a31c9581
James Talmage <james@talmage.io>
2016-01-02 03:53:01 -0500

    Extract `wrap-listener` utility.
    Can be used to create other plugins that manipulate require statements
    
    Squashed Commits:
    
    simplify attachment of expression sources
    
    split out emitter
    
    simplify listener
    
    refactor emitter with consistent argument ordering
    
    rename emitter to wrap-listener
    
    simplyify listener

--- index.js
+++ index.js
@@ -1,81 +1,30 @@
 'use strict';
-module.exports = detective;
-module.exports.metadata = extractMetadataFromResult;
+var wrapListener = require('./wrap-listener');
 
-function detective(babel) {
-	if (babel.Plugin) {
-		// Babel 5
-		return new babel.Plugin('detective', {visitor: {
-			ImportDeclaration: function (a, b, c, file) {
-				return visitImportDeclaration(this, file.opts.extra.detective, file);
-			},
-			CallExpression: function (a, b, c, file) {
-				return visitCallExpression(this, file.opts.extra.detective, file);
-			}
-		}});
-	}
-	// Babel 6
-	return {
-		visitor: {
-			ImportDeclaration: function (path, state) {
-				return visitImportDeclaration(path, state.opts, state.file);
-			},
-			CallExpression: function (path, state) {
-				return visitCallExpression(path, state.opts, state.file);
-			}
-		}
-	};
+function listener(path, file, opts) {
+	(path.isLiteral() ? addString : addExpression)(path.node, file, opts);
 }
 
-function visitImportDeclaration(path, opts, file) {
-	if (includeImports(opts)) {
-		addString(file, opts, path.node.source);
-	}
+function addString(node, file, opts) {
+	var val = attachNodes(opts) ? node : node.value;
+	requireMetadata(file).strings.push(val);
 }
 
-function visitCallExpression(path, opts, file) {
-	if (!includeRequire(opts)) {
-		return;
-	}
-	var callee = path.get('callee');
-	if (callee.isIdentifier() && callee.node.name === word(opts)) {
-		var arg = path.get('arguments.0');
-		if (arg && (!arg.isGenerated() || includeGenerated(opts))) {
-			if (arg.isLiteral()) {
-				addString(file, opts, arg.node);
-			}	else {
-				var loc = addExpression(file, opts, arg.node);
-				if (attachExpressionSource(opts)) {
-					loc.code = file.code.slice(loc.start, loc.end);
-				}
-			}
-		}
-	}
-}
-
-function extractMetadataFromResult(result) {
-	return result.metadata.requires;
-}
-
-function requireMetadata(file) {
-	var metadata = file.metadata;
-	return metadata.requires || (metadata.requires = {strings: [], expressions: []});
-}
-
-function addExpression(state, opts, node) {
+function addExpression(node, file, opts) {
 	var val;
 	if (attachNodes(opts)) {
 		val = node;
 	} else {
 		val = {start: node.start, end: node.end};
-		if (node.loc) {
-			val.loc = {
-				start: copyLoc(node.loc.start),
-				end: copyLoc(node.loc.end)
-			};
-		}
+		val.loc = {
+			start: copyLoc(node.loc.start),
+			end: copyLoc(node.loc.end)
+		};
 	}
-	requireMetadata(state).expressions.push(val);
+	if (attachExpressionSource(opts)) {
+		val.code = file.code.slice(val.start, val.end);
+	}
+	requireMetadata(file).expressions.push(val);
 	return val;
 }
 
@@ -83,28 +32,12 @@ function copyLoc(loc) {
 	return loc && {line: loc.line, column: loc.column};
 }
 
-function addString(state, opts, node) {
-	var val = attachNodes(opts) ? node : node.value;
-	requireMetadata(state).strings.push(val);
+function requireMetadata(file) {
+	var metadata = file.metadata;
+	return metadata.requires || (metadata.requires = {strings: [], expressions: []});
 }
 
 // OPTION EXTRACTION:
-
-function word(opts) {
-	return (opts && opts.word) || 'require';
-}
-
-function includeGenerated(opts) {
-	return Boolean(opts && opts.generated);
-}
-
-function includeImports(opts) {
-	return (!opts || opts.import) !== false;
-}
-
-function includeRequire(opts) {
-	return (!opts || opts.require) !== false;
-}
 
 function attachExpressionSource(opts) {
 	return Boolean(opts && opts.source);
@@ -113,3 +46,9 @@ function attachExpressionSource(opts) {
 function attachNodes(opts) {
 	return Boolean(opts && opts.nodes);
 }
+
+module.exports = wrapListener(listener, 'detective');
+
+module.exports.metadata = function extractMetadataFromResult(result) {
+	return result.metadata.requires;
+};
